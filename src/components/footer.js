8b8e679e9da9372b9ac91f0b3c45c2a36821d7b3
William Ford <wford1.home@gmail.com>
2021-05-23 15:12:15 +0100

    Finished landing page

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <footer className="container" style={{marginTop: "40px"}}>
+        <footer className="container footer-desktop is-hidden-touch">
             <div className="has-text-centered">
             <h1  className="has-text-weight-bold has-text-black"><i class="fab fa-react"></i> Code by William Ford</h1>
             </div>
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import hero from "../images/homepage.jpeg";
+import mobile from "../images/mobile.jpeg"
 import "../styles/helpers.css"
 
+
+
 const Hero = () => {
+  const [width] = useState(window.innerWidth)
+  
   return (
-    <div className="container is-fullhd is-hidden-touch">
-        <img className="center" src={hero} alt="" style={{position:"absolute", top: "-300px"}}/>
-      </div>
+    <div className="container is-fullhd">
+      {width >= 768 ? <img className="center" src={hero} alt="" style={{position:"absolute", top: "-300px"}}/> :
+      <img className="center" src={mobile} alt="" style={{position:"absolute", top:"-400px"}} />}
+        </div>
   );
 };
 
